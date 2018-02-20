8a6a131d98b7f5beeb407e606ee5174270d38af0
caminosajalberto <jalbercamino@gmail.com>
2018-02-20 10:55:32 +0100

    Subidos .m de la practica 1

---
+++ pr1/mcint.m
@@ -0,0 +1,24 @@
+function [valor] = mcint(fun, a, b,num_puntos)
+  
+  numAlX = rand(1, num_puntos) * (b - 1) + a;
+  
+  X=[a:0.001:b];
+  res = fun(X);
+  M = max(res);
+   
+  numAlY = rand(1, num_puntos) * M;
+  
+  resultados = fun(numAlX);
+  ndebajo = 0;
+  for i= 1: num_puntos
+    
+    if(numAlY(i) < resultados(i))
+      ndebajo = ndebajo + 1;
+    endif
+    
+  endfor
+  
+  valor = M * (b - a) * (ndebajo/num_puntos);
+   
+  
+  endfunction
---
+++ pr1/mcintvector.m
@@ -0,0 +1,18 @@
+function [valor] = mcintvector(fun, a, b,num_puntos)
+    
+  numAlX = rand(1, num_puntos) * (b - 1) + a;
+  
+  X=[a:0.001:b];
+  res = fun(X);
+  M = max(res);
+   
+  numAlY = rand(1, num_puntos) * M;
+  
+  resultados = fun(numAlX);
+  resta = numAlY - resultados;
+  ndebajo = sum(resta(:) < 0);
+  
+  valor = M * (b - a) * (ndebajo/num_puntos);
+   
+  
+  endfunction
