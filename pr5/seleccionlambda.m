67c3d317f402f1cb9149b48c6ad8e1bb9adba262
Pablo Arranz Ropero <arranzropablo@gmail.com>
2018-04-18 12:45:40 +0200

    funcion qe pinta learning curves para distintos lambda

---
+++ pr5/seleccionlambda.m
@@ -0,0 +1,48 @@
+function seleccionlambda(X, y, Xval, yval, Xtest, ytest, p)
+
+  lambda = [0, 0.001, 0.003, 0.01, 0.03, 0.1, 0.3, 1, 3, 10];
+
+  inittheta = ones(p + 1, 1);
+
+  X = generatepoldata(X, p);
+  Xval = generatepoldata(Xval, p);
+  Xtest = generatepoldata(Xtest, p);
+
+  [newX, mediaX, desvX] = featureNormalize(X);
+
+  Xval = bsxfun(@minus, Xval, mediaX);
+  newXval = bsxfun(@rdivide, Xval, desvX);
+
+  Xtest = bsxfun(@minus, Xtest, mediaX);
+  newXtest = bsxfun(@rdivide, Xtest, desvX);
+
+for i = 1:columns(lambda)
+
+    theta = fmincg(@(t)(coste(t, newX, y, lambda(i))),inittheta,optimset('GradObj', 'on', 'MaxIter', 200));
+    [Jtrain(i), grad] = coste(theta, newX, y, 0);
+
+    [Jval(i), grad] = coste(theta, newXval, yval, 0);
+
+endfor
+
+  plot(lambda, Jtrain, 'LineWidth', 2);
+  xlabel('lambda')
+  ylabel('Error')
+  hold on;
+  plot(lambda, Jval, 'LineWidth', 2);
+  hold off;
+
+  h = legend ({'Jtrain'}, 'Jval');
+  legend (h, 'location', 'northeastoutside');
+  set (h, 'fontsize', 20);
+
+#falta calcular el error con los datos de test, parece fallar al coger el minimo lambda, podemos verlo a ojo sino y hacer el codigo de debajo fuera
+#creo q es lo mejor, verlo a ojo, porque no se porque se no estan ordenados los Jval
+  [min, bestlambda] = min(Jval)
+  Jval
+
+  #theta = fmincg(@(t)(coste(t, newX, y, lambda(bestlambda))),inittheta,optimset('GradObj', 'on', 'MaxIter', 200));
+  #[Jtest, grad] = coste(theta, newXtest, ytest, lambda(bestlambda));
+  #bestlambda
+
+endfunction
