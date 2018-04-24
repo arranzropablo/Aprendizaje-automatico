0b3e32471bd852a4f305d0bb9f985152afecb43a
Pablo Arranz Ropero <arranzropablo@gmail.com>
2018-04-24 14:48:24 +0200

    creada funcion para pasar a .mat los datos, e incluidos los .mat

--- pr6/analyzeEmail.m
+++ pr6/analyzeEmail.m
@@ -1,18 +1,18 @@
 function asd = analyzeEmail()
 
-  #leemos y procesamos
-  file_contents = readFile('easy_ham/0024.txt');
-  email = processEmail(file_contents);
-
-  email_asd = strsplit(email);
-  vocabList = getVocabList();
-
-  X = ismember(vocabList, email_asd);
-
-  #esto de ismember crea un vector con 0s y 1s si esa palabra está, habría que hacer lo mismo para todos los correos una vez hecho lo q pone debajo
+  #leemos y procesamos (esto lo hace saveContentToMat, asi que aqui solo cargamos y hacemos el randomizado y separamos en los subsets)
 
   #habra que juntar spam con easy_spam (o con hard_spam, o con ambos, quizas hacer los 3 casos para ver cual va mejor) y luego moverlos aleatoriamente y coger 60-20-20
-  #y luego para cada correo ver que palabras del vocabulario tiene y ponerlas en los atributos y luego dependiendo si estaba en spam o en easy_ham o hard_ham poner la y a 0 o 1
+  
 
+  #data = load(name);
+  #[m,n] = size(data);
+  #idx = randperm(m);
+  #data_rand = data;
+  #data_rand(idx, :) = data;
+
+  #datatrain = data(1:floor(0.6*m),:)
+  #datacrossvalidation = data(floor(0.6*m):floor(0.8*m),:)
+  #datatest = data(floor(0.8*m):end,:)
 
 endfunction
